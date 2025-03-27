458a65b85adc43be1d14ffa961a3b937d2f12d88
TOM4NRY <eduardo.sanchez.toman@gmail.com>
2025-03-27 16:51:44 -0600

    ~ Cambios ~
        Funcionalidad:
    
        1. Ya se registran los datos del formulario en la tabla
        2. CRUD (botones "eliminar" y "editar") en cada fila
        2.1. Los botones aparecen automáticamente al añadir un tag
        3. El formulario se limpia al añadir un nuevo tag
    
        Estilo:
    
        1. Hice la tabla de los tags más grande para probar
        todos los datos a añadir
    
        2. Añadí una descripción como "select" para opciones comunes
        y controlar mejor la validación de datos.
        2.1 Añadi una opción "Otro" para que el usuario pueda
        escribir su propio tag
    
        3. Cambié nombres de variables y placeholders de acuerdo
        con su componente
    
        3. Letras azules en los componentes de números
        para resaltar que son medidas
    
    ~ Falta/Modificar ~
    
        Funcionalidad:
        - Conectar con BD
        - Que la página no se recargue al entrar en ella
        - La descripción libre (campo de texto) debe aparecer
        al seleciconar La opción "otro" del select
    
        Estilo:
        - Botones del CRUD
        - Diseño del valor

--- backend/src/js/tags.js
+++ backend/src/js/tags.js
@@ -1,4 +1,4 @@
-function validateInput(event) {
+function regExpDeviceIP(event) {
     const pattern = new RegExp("^[0-9.]*$");
     const input = event.key;
 
@@ -8,6 +8,8 @@ function validateInput(event) {
 
 
 let tagData, addTagRef, addRow, addCell
+
+
 const addTagForm = document.getElementById('addTagForm') // Get the form reference
 
 addTagForm.addEventListener("submit", function(event){
@@ -16,15 +18,13 @@ addTagForm.addEventListener("submit", function(event){
     addRowForm(tagData)
 })
 
+
 function addRowForm(tagData) {
     let tagTable = document.getElementById('tagTable') // Get the table reference
-    addRow = tagTable.insertRow(-1) // -1 = Add in the the last row
+    addRow = tagTable.insertRow(1) // -1 = Add in the the last row
 
     // Add the cells (colum ns) in the row
-    addCell = addRow.insertCell(0)
-    addCell.textContent = tagData.get('tagName')
-    addCell = addRow.insertCell(1)
-    addCell.textContent = tagData.get('tagIP')
-    addCell = addRow.insertCell(2)
-    addCell.textContent = tagData.get('tagType')
+    addCell = addRow.insertCell(0); addCell.textContent = tagData.get('tagName')
+    addCell = addRow.insertCell(1); addCell.textContent = tagData.get('tagAddress')
+    addCell = addRow.insertCell(2); addCell.textContent = tagData.get('tagType')
 }
---
+++ backend/src/js/tags2.js
@@ -0,0 +1,87 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.getElementById('fecha').value = new Date().toISOString().split('T')[0];
+// });
+
+let selectedRow = null
+
+function onFormSubmit() {
+    event.preventDefault();
+    let formData = readFormData()
+    if (selectedRow == null)
+        insertNewRecord(formData)
+    else
+        updateRecord(formData)
+    resetForm()
+}
+
+function readFormData() {
+    let formData = {}
+
+    formData["tagName"] = document.getElementById("name").value
+    formData["tagDescSelect"] = document.getElementById("dscSel").value
+    formData["tagAddress"] = document.getElementById("addr").value
+    formData["tagType"] = document.getElementById("type").value
+    formData["tagScale"] = document.getElementById("scl").value
+    formData["tagScan"] = document.getElementById("scn").value
+    formData["tagValue"] = document.getElementById("val").value
+
+    return formData
+}
+
+function insertNewRecord(data) {
+    let table = document.getElementById("tagTable").getElementsByTagName('tbody')[0]
+    let newRow = table.insertRow(-1) // -1 = Add in the last row
+
+    cell1 = newRow.insertCell(0); cell1.innerHTML = data.tagName
+    cell2 = newRow.insertCell(1); cell2.innerHTML = data.tagDescSelect
+    cell3 = newRow.insertCell(2); cell3.innerHTML = data.tagAddress
+    cell4 = newRow.insertCell(3); cell4.innerHTML = data.tagType
+    cell5 = newRow.insertCell(4); cell5.innerHTML = data.tagScale
+    cell6 = newRow.insertCell(5); cell6.innerHTML = data.tagScan
+    cell7 = newRow.insertCell(6); cell7.innerHTML = data.tagValue
+
+    cell8 = newRow.insertCell(7);
+    cell8.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+    <br> <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("name").value = ""
+    document.getElementById("dscSel").value = ""
+    document.getElementById("addr").value = ""
+    document.getElementById("type").value = ""
+    document.getElementById("scl").value = "1" // Minimum value
+    document.getElementById("scn").value = "1000" // Minimum value
+    document.getElementById("val").value = ""
+    
+    selectedRow = null
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement
+    document.getElementById("name").value = selectedRow.cells[0].innerHTML
+    document.getElementById("dscSel").value = selectedRow.cells[1].innerHTML
+    document.getElementById("addr").value = selectedRow.cells[2].innerHTML
+    document.getElementById("type").value = selectedRow.cells[3].innerHTML
+    document.getElementById("scl").value = selectedRow.cells[4].innerHTML
+    document.getElementById("scn").value = selectedRow.cells[5].innerHTML
+    document.getElementById("val").value = selectedRow.cells[6].innerHTML
+}
+
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.tagName
+    selectedRow.cells[1].innerHTML = formData.tagDescSelect
+    selectedRow.cells[2].innerHTML = formData.tagAddress
+    selectedRow.cells[3].innerHTML = formData.tagType
+    selectedRow.cells[4].innerHTML = formData.tagScale
+    selectedRow.cells[5].innerHTML = formData.tagScan
+    selectedRow.cells[6].innerHTML = formData.tagValue
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this row ?')) {
+        row = td.parentElement.parentElement
+        document.getElementById("tagTable").deleteRow(row.rowIndex)
+        resetForm()
+    }
+}
